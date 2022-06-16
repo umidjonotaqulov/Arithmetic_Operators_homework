0c2cd404202390436ac95ef27547db1a5de350cd
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-06-16 21:55:48 +0500

    arithmetic_op03.py

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,13 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the answer variable
+
+#"number" deb nomlangan o'zgaruvchi yarating va unga ikki xonali raqamni belgilang
+
+#Raqamning teskarisini toping va uni "answer" deb nomlangan o'zgaruvchiga belgilang.
+
+#Javob oʻzgaruvchisini chop eting
+
+number = 5
+answer = 1/number
+print(answer)
