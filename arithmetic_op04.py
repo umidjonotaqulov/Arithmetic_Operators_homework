d2473d200ff04360dab8f8ccdef90de15bc226a0
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-06-16 21:59:57 +0500

    arithmetic_op03

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -9,3 +9,26 @@
 #Create a variable called 'answer' and assign it the sum of the three digits.
 
 #print the sum of the three digits.
+
+#"number" deb nomlangan o'zgaruvchi yarating va unga uch xonali raqamni belgilang.
+
+#"number"ning birinchi raqamini toping va x1 ga belgilang.
+
+#"number"ning ikkinchi raqamini toping va x2 ga belgilang.
+
+#"number"ning uchinchi raqamini toping va x3 ga belgilang.
+
+#"answer" deb nomlangan o'zgaruvchi yarating va unga uchta raqam yig'indisini belgilang.
+
+#uchta raqamning yig'indisini chop eting.
+
+
+number = 857
+x1 = number % 10
+
+x2 = number // 10 % 10
+
+x3 = number // 100
+
+answer = x1 + x2 +x3 
+print( answer )
