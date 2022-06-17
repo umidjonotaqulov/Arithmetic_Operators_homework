c17d8fd99d9760cd3ec8e2e8cf32df7d06084a0c
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-06-17 05:47:55 +0500

    x1

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -7,15 +7,6 @@
 #Print the value of result.
 #'The result is:'
 
-#x o'zgaruvchisini yarating va unga 10 qiymatini belgilang.
-
-#y oʻzgaruvchisini yarating va unga 5 qiymatini belgilang.
-
-#O'zgaruvchi natija yarating va unga x + y qiymatini belgilang.
-
-#Natija qiymatini chop eting.
-#'result:'
-
 
 x = 10
 y = 5
